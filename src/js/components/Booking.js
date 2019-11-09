dc44d2406b70c8d82b082650c872be1bba53248b
Pawel Bozemski <paw.boz@gmail.com>
2019-11-09 11:20:50 +0100

    Add HourPicker.js and range-slider

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,7 @@
 import { select, templates } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 /* eslint-disable linebreak-style */
 class Booking {
@@ -21,6 +22,7 @@ class Booking {
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
   initWidgets(){
     const thisBooking = this;
@@ -28,6 +30,7 @@ class Booking {
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 
 }
